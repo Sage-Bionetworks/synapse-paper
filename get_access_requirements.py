43647843faa781ea6c30f7fde00c999b060623dd
Thomas Yu <thomas.yu@sagebase.org>
2025-09-03 17:36:13 -0700

    Add ARs

--- get_access_requirements.py
+++ get_access_requirements.py
@@ -2,6 +2,21 @@
 Description: a script to extract all access approvals for access requirements
 
 This script can only be executed by someone with ACT team privileges
+
+ADKP: 9603055
+ARK: 9605913
+ELITE LLFS DANISH IDU/DUC/IRB: 9606644
+NF-OSI:
+9606614 (no requests)
+9606610 (no requests)
+9606593 (no requests)
+9606557 (no requests)
+9606541
+9606508
+9605435
+9605422
+9605255
+9605240
 """
 
 import json
@@ -64,7 +79,21 @@ def main():
     # Set up a "readonly" synapse config profile
     syn = synapseclient.login(profile="readonly")
     all_submissions = []
-    access_requirements = [9603055, 9606628]
+    access_requirements = [
+        9603055,
+        9605913,
+        9606644,
+        9606614,
+        9606610,
+        9606593,
+        9606557,
+        9606541,
+        9606508,
+        9605435,
+        9605422,
+        9605255,
+        9605240,
+    ]
     for access_requirement in access_requirements:
         submission = get_submissions(syn=syn, access_requirement_id=access_requirement)
         all_submissions.extend(submission)
