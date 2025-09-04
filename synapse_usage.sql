4a2c9b08d2e8611d1febf1c0e7c56429e9632316
Thomas Yu <thomas.yu@sagebase.org>
2025-09-04 15:19:14 -0700

    Edit code

--- synapse_usage.sql
+++ synapse_usage.sql
@@ -1,7 +1,4 @@
-
-
 // petabytes uploaded over time (Figure 1)
-
 WITH year_storage AS (
   SELECT
     DATE_TRUNC('YEAR', created_on) AS created_on_year,
@@ -20,8 +17,8 @@ ORDER BY
   created_on_year ASC;
 
 
-// Figure 5 Number of unique Synapse account holders 2012-2024
-with cte as (
+// Number of unique Synapse account holders 2012-2024 (Figure 5)
+with user_created_on as (
     select
         YEAR(created_on) as created_on_yr,
         count(*) as number_of_users_created_accounts
@@ -37,7 +34,7 @@ select
     created_on_yr,
     sum(number_of_users_created_accounts) over (order by created_on_yr asc rows between unbounded preceding and current row) as number_of_users
 from
-    cte;
+    user_created_on;
 
 
 // number of downloads
@@ -55,7 +52,34 @@ where
     // TODO update this to only be files?
     project_id IS NOT NULL;
 
+// Total number of files in Synapse as of 08/01/2025
+with node_latest_before_2025_08_01 as (
+    SELECT
+        *
+    FROM
+        synapse_data_warehouse.synapse_event.node_event
+    WHERE
+        snapshot_timestamp >= date('2025-08-01') - interval '30 day'
+        and snapshot_timestamp < date('2025-08-01')
+    QUALIFY row_number() over (
+        partition by id
+        order by change_timestamp desc, snapshot_timestamp desc
+    ) = 1
+)
+SELECT
+    count(*) AS total_files
+FROM
+    node_latest_before_2025_08_01
+WHERE NOT (
+    change_type = 'DELETE'
+    OR benefactor_id = '1681355'
+    OR parent_id = '1681355'
+);
 
+select
+    count(*)
+from
+    synapse_data_warehouse.synapse.node_latest;
 
 // Synapse’s data governance infrastructure section
 // number of is_restricted / is_controlled / is_public combinations
@@ -91,7 +115,7 @@ from
     all_non_deleted_nodes_before_date;
 
     
-// program metadata
+// Table 4: storage volume before 08/01/2025
 
 // ADTR
 with adtr_snapshot_in_time as (
@@ -100,26 +124,39 @@ with adtr_snapshot_in_time as (
     from
         synapse_data_warehouse.synapse_event.node_event
     where
-        project_id = 2580853 and 
-        modified_on < DATE('2025-08-01')
+        modified_on < DATE('2025-09-04')
     QUALIFY ROW_NUMBER() OVER (
             PARTITION BY id
             ORDER BY modified_on DESC, snapshot_timestamp DESC
         ) = 1
+), adtr_latest as (
+    select
+        *
+    from
+        adtr_snapshot_in_time
+    WHERE
+        project_id = 2580853 and
+        NOT (adtr_snapshot_in_time.CHANGE_TYPE = 'DELETE' OR adtr_snapshot_in_time.BENEFACTOR_ID = '1681355' OR adtr_snapshot_in_time.PARENT_ID = '1681355') -- 1681355 is the synID of the trash can on Synapse
 )
 SELECT
-    count(distinct adtr_snapshot_in_time.id) as total_entities,
+    count(distinct adtr_latest.id) as total_entities,
     count(distinct FILE_LATEST.ID) as TOTAL_FILES,
     round(sum(FILE_LATEST.CONTENT_SIZE) / power(2, 40), 2) AS TOTAL_SIZE_IN_TB
 FROM
     SYNAPSE_DATA_WAREHOUSE.SYNAPSE.FILE_LATEST
 join
-    adtr_snapshot_in_time
-    on file_latest.id = adtr_snapshot_in_time.file_handle_id
-WHERE
-    NOT (adtr_snapshot_in_time.CHANGE_TYPE = 'DELETE' OR adtr_snapshot_in_time.BENEFACTOR_ID = '1681355' OR adtr_snapshot_in_time.PARENT_ID = '1681355'); -- 1681355 is the synID of the trash can on Synapse
+    adtr_latest
+    on file_latest.id = adtr_latest.file_handle_id;
+// NF-OSI
+// CCKP
+// dHealth
+// ARK
+// Project GENIE
+// ELITE
 
-// unique users
+
+// Table 4: unique data downloaders 1/1/2022 - 7/31/2025
+// ADTR
 select
     count(distinct user_id),
     min(record_date),
@@ -131,6 +168,15 @@ where
     association_object_type IS NOT NULL and
     project_id = 2580853 and 
     record_date < DATE('2025-08-01');
+
+// NF-OSI
+// CCKP
+// dHealth
+// ARK
+// Project GENIE
+// ELITE
+
+
 
 // Upload csv from get_access_requirements.py
 // access approval distribution
@@ -152,7 +198,7 @@ SELECT
     ) AS total_true
 FROM sage.scidata.access_approvals;
 
-
+// Detailed access approval table
 SELECT
     ACCESSREQUIREMENT_LATEST.NAME,
     ACCESSREQUIREMENT_LATEST.CONCRETE_TYPE,
@@ -160,7 +206,6 @@ SELECT
     ARRAY_SIZE(ACCESSORCHANGES) AS NUMBEROFREQUESTERS,
     ACCESS_APPROVALS.*,
     ACCESS_APPROVALS.researchProjectSnapshot:institution
-    -- modifiedon - submittedon as timetoresponse
 FROM
     SAGE.SCIDATA.ACCESS_APPROVALS
 JOIN
@@ -169,7 +214,6 @@ JOIN
 JOIN
     SYNAPSE_DATA_WAREHOUSE.SYNAPSE.ACCESSREQUIREMENT_LATEST
     ON ACCESS_APPROVALS.ACCESSREQUIREMENTID = ACCESSREQUIREMENT_LATEST.ID;
-
 
 // Institution distribution of access approvals
 select
@@ -180,21 +224,18 @@ from
 group by
     institution;
 
-
+// Number of access approvals by access requirement
 SELECT
     ACCESSREQUIREMENT_LATEST.NAME,
-    count(*)
+    count(*) as number_of_access_approvals
     -- modifiedon - submittedon as timetoresponse
 FROM
     SAGE.SCIDATA.ACCESS_APPROVALS
 JOIN
-    SYNAPSE_DATA_WAREHOUSE.SYNAPSE.USERPROFILE_LATEST
-    ON ACCESS_APPROVALS.SUBMITTEDBY = USERPROFILE_LATEST.ID
-JOIN
     SYNAPSE_DATA_WAREHOUSE.SYNAPSE.ACCESSREQUIREMENT_LATEST
     ON ACCESS_APPROVALS.ACCESSREQUIREMENTID = ACCESSREQUIREMENT_LATEST.ID
 group by
-    ACCESSREQUIREMENT_LATEST.NAME
+    ACCESSREQUIREMENT_LATEST.NAME;
 
 
 // unused metrics
